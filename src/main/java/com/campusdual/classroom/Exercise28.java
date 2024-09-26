d6f50371260af83a0023f1fc9f5796bda4ee8edb
JoseMRLC <josemriveiroticbgmail.com>
2024-09-26 13:51:22 +0200

    ejercicio resuelto v2

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -2,6 +2,6 @@ package com.campusdual.classroom;
 
 public class Exercise28 {
     public static void main(String[] args) throws Exception {
-      throw new Exception("Excepcion de prueba");
+      throw new Exception("Excepcion de prueba v2");
     }
 }
